1bc350c8c8d1807b3cccf090896739d7ce0c6170
Ben Yu <intenex@gmail.com>
2019-03-23 23:03:18 -0700

    Complete many hand specs

--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -40,29 +40,85 @@ describe Hand do
     end
 
     describe "#initialize" do
-      it "stores the cards passed to it"
+      cards = [
+        Card.new(:hearts, :four),
+        Card.new(:diamonds, :deuce),
+        Card.new(:clubs, :ace),
+        Card.new(:hearts, :jack),
+        Card.new(:spades, :king)
+      ]
+      hand = Hand.new(cards.dup)
 
-      it "sorts the cards"
+      it "stores the cards passed to it" do
+        expect(hand.cards).to match_array(cards)
+      end
+
+      it "sorts the cards" do
+        expect(hand.cards).to eq(cards.sort_by { |card| Card.values.index(card.value) })
+      end
     end
 
     describe "#has_rank?" do
-      it "returns true if any card in the hand matches the rank passed"
+      cards = [
+        Card.new(:hearts, :four),
+        Card.new(:diamonds, :deuce)
+      ]
+      hand = Hand.new(cards)
 
-      it "returns false if no card in the hand matches the rank passed"
+      it "returns true if any card in the hand matches the rank passed" do
+        expect(hand.has_rank?(:four)).to be(true)
+        expect(hand.has_rank?(:deuce)).to be(true)
+      end
+
+      it "returns false if no card in the hand matches the rank passed" do
+        expect(hand.has_rank?(:five)).to be(false)
+        expect(hand.has_rank?(:ace)).to be(false)
+      end
     end
 
     describe "#add_cards" do
-      it "adds the passed cards to the current hand of cards"
+      cards = [
+        Card.new(:hearts, :four),
+        Card.new(:diamonds, :deuce)
+      ]
+      hand = Hand.new(cards.dup)
+      new_cards = [
+        Card.new(:diamonds, :five),
+        Card.new(:spades, :ace)
+      ]
+      hand.add_cards(new_cards)
+      it "adds the passed cards to the current hand of cards" do
+        expect(hand.cards).to match_array(cards + new_cards)
+      end
 
-      it "sorts all the cards after adding the new cards"
+      it "sorts all the cards after adding the new cards" do
+        expect(hand.cards).to eq(cards.concat(new_cards).sort_by { |card| Card.values.index(card.value) })
+      end
     end
 
     describe "#remove_cards" do
-      it "removes all the cards of the passed value from the hand"
+      cards = [
+        Card.new(:hearts, :four),
+        Card.new(:diamonds, :four),
+        Card.new(:clubs, :ace),
+        Card.new(:hearts, :ace),
+        Card.new(:spades, :king)
+      ]
+      hand = Hand.new(cards.dup)
+      it "removes all the cards of the passed value from the hand" do
+        hand.remove_cards(:four)
+        new_cards = cards.reject { |card| card.value == :four }
+        expect(hand.cards).to match_array(new_cards)
+      end
 
-      it "returns all the cards of the passed value that were removed"
+      it "returns all the cards of the passed value that were removed" do
+        removed_cards = cards.select { |card| card.value == :ace }
+        expect(hand.remove_cards(:ace)).to match_array(removed_cards)
+      end
 
-      it "returns nil if no cards in the hand match the passed value"
+      it "returns nil if no cards in the hand match the passed value" do
+        expect(hand.remove_cards(:jack)).to be(nil)
+      end
     end
 
     describe "#book" do
